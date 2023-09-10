508d1b6edacf29f00dd688be4d5293bf0d3076b3
Michał <brysz.michal@gmail.com>
2023-09-10 12:42:17 +0200

    Excercise

--- AdvancedArrays/exercise5.js
+++ AdvancedArrays/exercise5.js
@@ -4,37 +4,36 @@ const array = [
     username: "john",
     team: "red",
     score: 5,
-    items: ["ball", "book", "pen"]
+    items: ["ball", "book", "pen"],
   },
   {
     username: "becky",
     team: "blue",
     score: 10,
-    items: ["tape", "backpack", "pen"]
+    items: ["tape", "backpack", "pen"],
   },
   {
     username: "susy",
     team: "red",
     score: 55,
-    items: ["ball", "eraser", "pen"]
+    items: ["ball", "eraser", "pen"],
   },
   {
     username: "tyson",
     team: "green",
     score: 1,
-    items: ["book", "pen"]
+    items: ["book", "pen"],
   },
-
 ];
 
 //Create an array using forEach that has all the usernames with a "!" to each of the usernames
-
+const exclArray = array.forEach((num) => {
+  
+}
 
 //Create an array using map that has all the usernames with a "? to each of the usernames
 
-
 //Filter the array to only include users who are on team: red
-
 
 //Find out the total score of all users using reduce
 
@@ -42,9 +41,9 @@ const array = [
 // (2), Make this map function pure:
 const arrayNum = [1, 2, 4, 5, 8, 9];
 const newArray = arrayNum.map((num, i) => {
-	console.log(num, i);
-	alert(num);
-	return num * 2;
-})
+  console.log(num, i);
+  alert(num);
+  return num * 2;
+});
 
 //BONUS: create a new list with all user information, but add "!" to the end of each items they own.
