bdaf74bf378324c9ff075a2bebdc7a72ad526f59
Michał <brysz.michal@gmail.com>
2023-09-19 23:43:19 +0200

    ex update

--- advanced_exercise_es10/ES10-Exercise.js
+++ advanced_exercise_es10/ES10-Exercise.js
@@ -33,13 +33,14 @@ console.log(joinedString);
 
 //#4 Turn the trapped 3 number into: [3]
 const trapped = [[[[[[[[[[[[[[[[[[[[[[[[[[3]]]]]]]]]]]]]]]]]]]]]]]]]];
+trapped.flat(25);
 
 //#5 Clean up this email to have no whitespaces. Make the answer be in a single line (return a new string):
 const userEmail3 = "     cannotfillemailformcorrectly@gmail.com   ";
-
+console.log(userEmail3.trimStart().trimEnd());
 //#6 Turn the below users (value is their ID number) into an array: [ [ 'user1', 18273 ], [ 'user2', 92833 ], [ 'user3', 90315 ] ]
 const users = { user1: 18273, user2: 92833, user3: 90315 };
-
+Object.entries(users);
 //#7 change the output array of the above to have the user's IDs multiplied by 2 -- Should output:[ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]
 
 //#8 change the output array of question #7 back into an object with all the users IDs updated to their new version. Should output: { user1: 36546, user2: 185666, user3: 180630 }
