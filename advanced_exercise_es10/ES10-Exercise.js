580ad405df6903f0ea19e73437e65e4dffa3b8c8
Michał <brysz.michal@gmail.com>
2023-09-24 21:11:13 +0200

    finish ex10

--- advanced_exercise_es10/ES10-Exercise.js
+++ advanced_exercise_es10/ES10-Exercise.js
@@ -40,7 +40,12 @@ const userEmail3 = "     cannotfillemailformcorrectly@gmail.com   ";
 console.log(userEmail3.trimStart().trimEnd());
 //#6 Turn the below users (value is their ID number) into an array: [ [ 'user1', 18273 ], [ 'user2', 92833 ], [ 'user3', 90315 ] ]
 const users = { user1: 18273, user2: 92833, user3: 90315 };
-Object.entries(users);
+let arrUsers = Object.entries(users);
+console.log(arrUsers);
 //#7 change the output array of the above to have the user's IDs multiplied by 2 -- Should output:[ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]
-
+const modifiedArray = arrUsers.map(([userId, value]) => [userId, value * 2]);
+console.log(modifiedArray);
 //#8 change the output array of question #7 back into an object with all the users IDs updated to their new version. Should output: { user1: 36546, user2: 185666, user3: 180630 }
+const modifiedArray = arrUsers.map(([userId, value]) => [userId, value * 2]);
+console.log(modifiedArray);
+Object.fromEntries(modifiedArray);
